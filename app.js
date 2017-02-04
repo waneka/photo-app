819febee5c15673ec3c06d2076c51ac3657183ac
waneka <twwaneka@gmail.com>
2017-02-04 10:00:11 -0800

    Fetch photos on app init, pass response data to handler function

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 var app = (function () {
+  var pixabayPublicKey = '4424166-feb46d87eba60cf69cbaa833f';
+
+  function handleResponseData(responseData) {
+    var photos = responseData.hits;
+
+    console.log('photos: ', photos);
+  }
+
+  function fetchPhotos() {
+    var photoRequest = new XMLHttpRequest();
+    var endpoint;
+    var responseData;
+
+    photoRequest.onreadystatechange = function () {
+      if (this.readyState === XMLHttpRequest.DONE) {
+        if (this.status === 200) {
+          responseData = JSON.parse(this.responseText);
+          handleResponseData(responseData);
+        } else {
+          // display error
+        }
+      }
+    }
+
+    endpoint = 'https://pixabay.com/api/?key=' + pixabayPublicKey;
+    photoRequest.open('GET', endpoint);
+    photoRequest.send();
+  }
 
   function init() {
-    console.log('initializing app');
+    fetchPhotos();
   }
 
   return {
